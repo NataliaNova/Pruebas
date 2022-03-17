734e6536d50f2ea4155469f790dd5e953de48bbd
NataliaNova <natnov319@gmail.com>
2022-03-17 12:37:35 -0500

    Taller17.03.2022

---
+++ clase 1 Flex/js/script.js
@@ -0,0 +1,31 @@
+/* let consolas = ["xbox", "ps5", "nitendo"];
+console.log("mi arreglo contiene" + " " + consolas);
+console.log(consolas.length);
+console.log("Estoy en la posicion" + " " + consolas[1]);
+
+let ulimo = consolas[consolas.length - 1];
+console.log("Soy el último" + " " + ulimo);
+
+consolas.forEach(function (elemento, indice) {
+  console.log("recorrido por forEach" + elemento, indice);
+});
+
+consolas.forEach((elemento) => console.log("soy funcion flecha ") + elemento);
+
+let nuevoElemento= consolas.push('sega');
+console.log("Estoy con un nuevo elemento" + " " + nuevoElemento);
+
+ulimo = consolas[consolas.length - 1];
+console.log("Soy el nuevo último" + " " + ulimo);
+
+//Objetos
+const persona ={
+    name: "Natalia",
+    Lastname: "Nov",
+    Age: 26,
+};
+
+console.log("imprimo desde un objeto"+ persona.name);
+ */
+
+
